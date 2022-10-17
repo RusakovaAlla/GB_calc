7011d5a884c71117d624f66c6a18189cd217da77
SergeySA12 <strenadkosa@mail.ru>
2022-10-17 20:32:01 +0300

    logger_calc.py

--- logger_calc.py
+++ logger_calc.py
@@ -2,7 +2,7 @@ from datetime import datetime as dt
 from time import time
 
 
-def result_logger(data, result):
+def result_logger(time, result):
     time = dt.now().strftime('%H:%M')
     with open('log.csv', 'a') as file:
         file.write('{} результат :{}\n'.format(
