50afa6d84911c966eb8284d61536ee7777b4b304
Irina <ira.nesheret@yandex.ru>
2022-10-19 22:47:29 +0300

    commit_

--- calc_complex_numbers.py
+++ calc_complex_numbers.py
@@ -1,19 +1,13 @@
 # Блок для расчетов операций с комплексными числами
 
-# import cmath
-
-from math import nan
-
 
 def Calc_block(left_value, oper, right_value):
     if oper == '+':
         res = sum(left_value, right_value)
-    if oper == '-':
+    elif oper == '-':
         res = sub(left_value, right_value)
-    if oper == '*':
+    elif oper == '*':
         res = mult(left_value, right_value)
-    if (oper =='/') and (right_value != 0):
-        res = div(left_value, right_value)
     else:
         res = 'Ошибка деления на 0!'
     return res
@@ -30,10 +24,9 @@ def sub(left_value: complex, right_value: complex):
 def mult(left_value: complex, right_value: complex):
     return left_value * right_value
 
-
 def div(left_value: complex, right_value: complex):
     return left_value / right_value
 
 a = 0 + 0j
 b = 0 + 0j
-print(Calc_block(a,'/', b))
+print(Calc_block(a, '/', b))
